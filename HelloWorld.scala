17c261eaba143f3e8888fd1d23914072148caf92
Abhimanyu Srivastava <srabhimanyu@gmail.com>
2020-05-23 22:20:53 +0530

    Update HelloWorld.scala

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -6,3 +6,16 @@ object HelloWorld {
       println("Hello, world!") // prints Hello World
    }
 }
+
+
+object HelloWorld {
+   /* This is my first java program.  
+    * This will print 'Hello World' as the output
+    * This is an example of multi-line comments.
+    */
+   def main(args: Array[String]) {
+      // Prints Hello World
+      // This is also an example of single line comment.
+      println("Hello, world!") 
+   }
+}
